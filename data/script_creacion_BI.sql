2923a5344e4a583b02534078ae32975076fe3844
fedecarracedo <ftcarracedo@gmail.com>
2023-12-02 01:58:04 -0300

    Avances con las migraciones y las vistas

--- data/script_creacion_BI.sql
+++ data/script_creacion_BI.sql
@@ -1,3 +1,50 @@
+-- Funciones auxiliares
+
+CREATE FUNCTION DATA_TEAM.Calcular_cuatri(@fecha smalldatetime)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @cuatri INT
+	SET @cuatri = CEILING((MONTH(@fecha) / 4.0))
+	RETURN @cuatri
+END
+GO
+
+CREATE FUNCTION DATA_TEAM.ObtenerRangoEtario(@fechaNacimiento DATE)
+RETURNS INT
+AS
+BEGIN
+    DECLARE @rangoEtario INT
+
+    SELECT @rangoEtario = RANGO_ETARIO_ID
+    FROM (
+        SELECT RANGO_ETARIO_ID, CAST(EDAD_INICIO AS INT) AS EdadInicio, CAST(EDAD_FIN AS INT) AS EdadFin
+        FROM DATA_TEAM.BI_D_RANGO_ETARIO
+    ) AS Rangos
+    WHERE DATEDIFF(YEAR, @fechaNacimiento, GETDATE()) >= EdadInicio AND DATEDIFF(YEAR, @fechaNacimiento, GETDATE()) < EdadFin
+
+    RETURN @rangoEtario
+END
+GO
+
+CREATE FUNCTION DATA_TEAM.ObtenerRangoMetros(@superficie numeric(18,2))
+RETURNS INT
+AS
+BEGIN
+    DECLARE @rangoMetros INT
+
+    SELECT @rangoMetros = RANGO_METROS_ID
+    FROM (
+        SELECT RANGO_METROS_ID, RANGO_METROS_INICIO AS RangoInicio, RANGO_METROS_FIN AS RangoFin
+        FROM DATA_TEAM.BI_D_RANGO_METROS
+    ) AS Rangos
+    WHERE @superficie >= RangoInicio AND @superficie < RangoFin
+
+    RETURN @rangoMetros
+END
+GO
+
+
 -- Creación de Tablas
 
 
@@ -28,7 +75,7 @@ CREATE TABLE DATA_TEAM.BI_D_LOCALIDAD (
 GO
 
 CREATE TABLE DATA_TEAM.BI_D_RANGO_ETARIO (
-    RANGO_ETARIO_ID int NOT NULL PRIMARY KEY,
+    RANGO_ETARIO_ID int IDENTITY(1,1) NOT NULL PRIMARY KEY,
     EDAD_INICIO decimal(18,0),
     EDAD_FIN decimal(18,0)
 )
@@ -45,9 +92,9 @@ CREATE TABLE DATA_TEAM.BI_D_TIPO_OPERACION (
 GO
 
 CREATE TABLE DATA_TEAM.BI_D_RANGO_METROS (
-    RANGO_METROS_ID int PRIMARY KEY,
-    RANGO_METROS_INICIO decimal(12,2),
-    RANGO_METROS_FIN decimal(12,2)
+    RANGO_METROS_ID int IDENTITY(1,1) PRIMARY KEY,
+    RANGO_METROS_INICIO numeric(18,2),
+    RANGO_METROS_FIN numeric(18,2)
 )
 GO
 
@@ -111,6 +158,7 @@ CREATE TABLE DATA_TEAM.BI_H_VENTA (
     VENTA_LOCALIDAD NVARCHAR(100) NOT NULL FOREIGN KEY REFERENCES DATA_TEAM.BI_D_LOCALIDAD,
     VENTA_RANGO_ETARIO int NOT NULL FOREIGN KEY REFERENCES DATA_TEAM.BI_D_RANGO_ETARIO,
     VENTA_TIPO_INMUEBLE NVARCHAR(100) NOT NULL FOREIGN KEY REFERENCES DATA_TEAM.BI_D_TIPO_INMUEBLE,
+	VENTA_TIPO_OPERACION NVARCHAR(100) NOT NULL FOREIGN KEY REFERENCES DATA_TEAM.BI_D_TIPO_OPERACION,
     VENTA_MONEDA NVARCHAR(100) NOT NULL FOREIGN KEY REFERENCES DATA_TEAM.BI_D_MONEDA,
     VENTA_RANGO_METROS int NOT NULL FOREIGN KEY REFERENCES DATA_TEAM.BI_D_RANGO_METROS,
     VENTA_ANIO INT NOT NULL,
@@ -118,8 +166,7 @@ CREATE TABLE DATA_TEAM.BI_H_VENTA (
     VENTA_MES INT NOT NULL,
     TOTAL_VENTAS decimal(12,2),
     TOTAL_COMISION decimal(12,2),
-    CANTIDAD_VENTAS decimal(12,0),
-    TOTAL_CIERRE_OP decimal(12,2)
+    CANTIDAD_VENTAS decimal(12,0)
 )
 GO
 
@@ -228,6 +275,287 @@ BEGIN
 END
 GO
 
+CREATE PROCEDURE DATA_TEAM.MIGRAR_D_RANGO_ETARIO
+AS
+BEGIN
+    INSERT INTO DATA_TEAM.BI_D_RANGO_ETARIO(EDAD_INICIO, EDAD_FIN) VALUES
+    (0,25),
+    (25,35),
+    (35,50),
+    (50,100)
+END
+GO
+
+CREATE PROCEDURE DATA_TEAM.MIGRAR_D_RANGO_METROS
+AS
+BEGIN
+    INSERT INTO DATA_TEAM.BI_D_RANGO_METROS(RANGO_METROS_INICIO, RANGO_METROS_FIN) VALUES
+    (0,35),
+    (35,55),
+    (55,75),
+    (75,100)
+END
+GO
+
+CREATE PROC DATA_TEAM.MIGRAR_H_ANUNCIO AS
+BEGIN
+    INSERT INTO DATA_TEAM.BI_H_ANUNCIO(
+		ANUNCIO_BARRIO,
+		ANUNCIO_TIPO_INMUEBLE,
+		ANUNCIO_MONEDA,
+		ANUNCIO_TIPO_OPERACION,
+		ANUNCIO_RANGO_METROS,
+		ANUNCIO_ANIO,
+		ANUNCIO_CUATRI,
+		ANUNCIO_MES,
+		ANUNCIO_AMBIENTES,
+		TOTAL_PRECIO,
+		TOTAL_TIEMPO_ALTA
+	)
+	SELECT INMUEBLE_BARRIO,
+	INMUEBLE_TIPO_INMUEBLE,
+	ANUNCIO_MONEDA,
+	ANUNCIO_TIPO_OPERACION,
+	YEAR(ANUNCIO_FECHA_PUBLICACION),
+	DATA_TEAM.Calcular_cuatri(ANUNCIO_FECHA_PUBLICACION),
+	MONTH(ANUNCIO_FECHA_PUBLICACION),
+	INMUEBLE_CANT_AMBIENTES,
+	SUM(ANUNCIO_COSTO_ANUNCIO),
+	DATEDIFF(DAY, ANUNCIO_FECHA_PUBLICACION, ANUNCIO_FECHA_FINALIZACION)
+	FROM DATA_TEAM.Anuncio
+	JOIN DATA_TEAM.Inmueble ON INMUEBLE_CODIGO = ANUNCIO_INMUEBLE
+	GROUP BY
+	INMUEBLE_BARRIO,
+	INMUEBLE_TIPO_INMUEBLE,
+	ANUNCIO_MONEDA,
+	ANUNCIO_TIPO_OPERACION,
+	YEAR(ANUNCIO_FECHA_PUBLICACION),
+	DATA_TEAM.Calcular_cuatri(ANUNCIO_FECHA_PUBLICACION),
+	MONTH(ANUNCIO_FECHA_PUBLICACION),
+	INMUEBLE_CANT_AMBIENTES
+END
+GO
+
+CREATE PROC DATA_TEAM.MIGRAR_H_VENTA AS
+BEGIN
+    INSERT INTO DATA_TEAM.BI_H_VENTA(
+		VENTA_SUCURSAL,
+		VENTA_LOCALIDAD,
+		VENTA_RANGO_ETARIO,
+		VENTA_TIPO_INMUEBLE,
+		VENTA_MONEDA,
+		VENTA_RANGO_METROS,
+		VENTA_ANIO,
+		VENTA_CUATRI,
+		VENTA_MES,
+		TOTAL_VENTAS,
+		TOTAL_COMISION,
+		CANTIDAD_VENTAS
+	)
+	SELECT 
+	SUCURSAL_CODIGO,
+	SUCURSAL_LOCALIDAD,
+	DATA_TEAM.ObtenerRangoEtario(AGENTE_FECHA_NAC),
+	INMUEBLE_TIPO_INMUEBLE,
+	VENTA_MONEDA,
+	DATA_TEAM.ObtenerRangoMetros(INMUEBLE_SUPERFICIETOTAL),
+	YEAR(VENTA_FECHA),
+	DATA_TEAM.Calcular_cuatri(VENTA_FECHA),
+	MONTH(VENTA_FECHA),
+	SUM(VENTA_PRECIO_VENTA),
+	SUM(VENTA_COMISION),
+	COUNT(DISTINCT VENTA_CODIGO)
+	FROM DATA_TEAM.Venta
+	JOIN DATA_TEAM.Anuncio ON VENTA_ANUNCIO = ANUNCIO_CODIGO
+	JOIN DATA_TEAM.Agente ON ANUNCIO_AGENTE = AGENTE_DNI
+	JOIN DATA_TEAM.Sucursal ON AGENTE_SUCURSAL = SUCURSAL_CODIGO
+	JOIN DATA_TEAM.Inmueble ON ANUNCIO_INMUEBLE = INMUEBLE_CODIGO
+	GROUP BY
+	SUCURSAL_CODIGO,
+	SUCURSAL_LOCALIDAD,
+	DATA_TEAM.ObtenerRangoEtario(AGENTE_FECHA_NAC),
+	INMUEBLE_TIPO_INMUEBLE,
+	VENTA_MONEDA,
+	DATA_TEAM.ObtenerRangoMetros(INMUEBLE_SUPERFICIETOTAL),
+	YEAR(VENTA_FECHA),
+	DATA_TEAM.Calcular_cuatri(VENTA_FECHA),
+	MONTH(VENTA_FECHA)
+END
+GO
+
+------------------- Creacion de vistas
+
+ ---------------  1 
+/*Duración promedio (en días) que se encuentran publicados los anuncios
+según el tipo de operación (alquiler, venta, etc), barrio y ambientes para cada
+cuatrimestre de cada año. Se consideran todos los anuncios que se dieron de alta
+en ese cuatrimestre. La duración se calcula teniendo en cuenta la fecha de alta y
+la fecha de ﬁnalización.*/
+GO
+CREATE VIEW VistaDuracionPromedio AS
+SELECT
+    ANUNCIO_CODIGO,
+    ANUNCIO_TIPO_OPERACION AS TipoOperacion,
+    ANUNCIO_BARRIO AS Barrio,
+    ANUNCIO_AMBIENTES AS Ambientes,
+    ANUNCIO_ANIO AS Anio,
+    ANUNCIO_CUATRI AS Cuatrimestre,
+    DATEDIFF(day, Fecha_Alta, Fecha_Finalizacion) AS DuracionEnDias,
+    AVG(DATEDIFF(day, Fecha_Alta, Fecha_Finalizacion)) AS DuracionPromedioEnDias
+FROM (
+    SELECT
+        ANUNCIO_CODIGO,
+        ANUNCIO_TIPO_OPERACION,
+        ANUNCIO_BARRIO,
+        ANUNCIO_AMBIENTES,
+        ANUNCIO_ANIO,
+        ANUNCIO_CUATRI,
+        ANUNCIO_FECHA_ALTA AS Fecha_Alta,
+        ANUNCIO_FECHA_FINALIZACION AS Fecha_Finalizacion
+    FROM
+        DATA_TEAM.BI_H_ANUNCIO
+) AS Anuncios
+GROUP BY
+    ANUNCIO_CODIGO,
+    ANUNCIO_TIPO_OPERACION,
+    ANUNCIO_BARRIO,
+    ANUNCIO_AMBIENTES,
+    ANUNCIO_ANIO,
+    ANUNCIO_CUATRI;
+
+
+ ---------------  2 
+ /* Precio promedio de los anuncios de inmuebles según el tipo de operación
+(alquiler, venta, etc), tipo de inmueble y rango m2 para cada cuatrimestre/año.
+Se consideran todos los anuncios que se dieron de alta en ese cuatrimestre. El
+precio se debe expresar en el tipo de moneda que corresponda, identiﬁcando de
+cuál se trata.*/
+GO
+CREATE VIEW VistaPrecioPromedio AS
+SELECT
+    ANUNCIO_TIPO_OPERACION AS TipoOperacion,
+    ANUNCIO_TIPO_INMUEBLE AS TipoInmueble,
+    ANUNCIO_RANGO_METROS AS RangoMetros,
+    ANUNCIO_ANIO AS Anio,
+    ANUNCIO_CUATRI AS Cuatrimestre,
+    ANUNCIO_MONEDA AS Moneda,
+    AVG(TOTAL_PRECIO) AS PrecioPromedio
+FROM
+    DATA_TEAM.BI_H_ANUNCIO
+GROUP BY
+    ANUNCIO_TIPO_OPERACION,
+    ANUNCIO_TIPO_INMUEBLE,
+    ANUNCIO_RANGO_METROS,
+    ANUNCIO_ANIO,
+    ANUNCIO_CUATRI,
+    ANUNCIO_MONEDA;
+
+  ---------------  3
+  /* Los 5 barrios más elegidos para alquilar en función del rango etario de los
+inquilinos para cada cuatrimestre/año. Se calcula en función de los alquileres
+dados de alta en dicho periodo.*/
+
+  CREATE VIEW Top5BarriosAlquiler AS
+SELECT
+    TOP 5 WITH TIES
+    ALQUILER_BARRIO AS Barrio,
+    RANGO_ETARIO_ID AS RangoEtario,
+    ALQUILER_ANIO AS Anio,
+    ALQUILER_CUATRI AS Cuatrimestre,
+    COUNT(*) AS TotalAlquileres
+FROM
+    DATA_TEAM.BI_H_ALQUILER A
+JOIN
+    DATA_TEAM.BI_D_RANGO_ETARIO R ON A.ALQUILER_RANGO_ETARIO = R.RANGO_ETARIO_ID
+GROUP BY
+    ALQUILER_BARRIO,
+    RANGO_ETARIO_ID,
+    ALQUILER_ANIO,
+    ALQUILER_CUATRI
+ORDER BY
+    ALQUILER_ANIO,
+    ALQUILER_CUATRI,
+    TotalAlquileres DESC;
+
+    ---------------  4
+/* Porcentaje de incumplimiento de pagos de alquileres en término por cada
+mes/año. Se calcula en función de las fechas de pago y fecha de vencimiento del
+mismo. El porcentaje es en función del total de pagos en dicho periodo.*/
+CREATE VIEW PorcentajeIncumplimientoPagos AS
+SELECT
+    A.ALQUILER_CODIGO,
+    YEAR(PA.PAGO_ALQUILER_FECHA) AS Anio,
+    MONTH(PA.PAGO_ALQUILER_FECHA) AS Mes,
+    100.0 * SUM(CASE WHEN PA.PAGO_ALQUILER_FECHA > PA.PAGO_ALQUILER_FECHA_VENCIMIENTO THEN 1 ELSE 0 END) / COUNT(*) AS PorcentajeIncumplimiento
+FROM
+    DATA_TEAM.Alquiler A
+INNER JOIN
+    DATA_TEAM.Pago_alquiler PA ON A.ALQUILER_CODIGO = PA.ALQUILER_CODIGO
+GROUP BY
+    A.ALQUILER_CODIGO,
+    YEAR(PA.PAGO_ALQUILER_FECHA),
+    MONTH(PA.PAGO_ALQUILER_FECHA);
+
+
+    ---------------  5
+/* Porcentaje promedio de incremento del valor de los alquileres para los
+contratos en curso por mes/año. Se calcula tomando en cuenta el último pago
+con respecto al del mes en curso, únicamente de aquellos alquileres que hayan
+tenido aumento y están activos.*/
+
+SELECT 
+    A.ALQUILER_CODIGO,
+    YEAR(PA_ACTUAL.PAGO_ALQUILER_FECHA) AS ALQUILER_ANIO,
+    MONTH(PA_ACTUAL.PAGO_ALQUILER_FECHA) AS ALQUILER_MES,
+    AVG((PA_ACTUAL.PAGO_ALQUILER_IMPORTE - PA_ANTERIOR.PAGO_ALQUILER_IMPORTE) / PA_ANTERIOR.PAGO_ALQUILER_IMPORTE) AS Porcentaje_Incremento
+FROM DATA_TEAM.Alquiler A
+JOIN DATA_TEAM.Pago_alquiler PA_ACTUAL ON A.ALQUILER_CODIGO = PA_ACTUAL.PAGO_ALQUILER_CODIGO
+LEFT JOIN DATA_TEAM.Pago_alquiler PA_ANTERIOR ON A.ALQUILER_CODIGO = PA_ANTERIOR.PAGO_ALQUILER_CODIGO
+    AND PA_ACTUAL.PAGO_ALQUILER_FECHA > PA_ANTERIOR.PAGO_ALQUILER_FECHA
+WHERE 
+    A.ALQUILER_ESTADO = 'Alquilado' -- Alquileres activos
+    AND PA_ANTERIOR.PAGO_ALQUILER_CODIGO IS NOT NULL -- Verifico que tengan al menos dos pagos
+    AND PA_ACTUAL.PAGO_ALQUILER_IMPORTE > PA_ANTERIOR.PAGO_ALQUILER_IMPORTE -- Alquileres con incremento, el resto descarto
+GROUP BY 
+    A.ALQUILER_CODIGO,
+    YEAR(PA_ACTUAL.PAGO_ALQUILER_FECHA),
+    MONTH(PA_ACTUAL.PAGO_ALQUILER_FECHA)
+ORDER BY 
+    ALQUILER_ANIO, ALQUILER_MES;
+
+    ---------------  6
+    /* Valor promedio de la comisión según el tipo de operación (alquiler, venta, etc)
+y sucursal para cada cuatrimestre/año. Se calcula en función de los alquileres y
+ventas concretadas dentro del periodo. 
+ COALESCE DEVUELVE EL PRIMER ELEMENTO NO NULL entre ellos dos*/
+    SELECT 
+    CASE 
+        WHEN V.VENTA_TIPO_OPERACION IS NOT NULL THEN 'Venta'
+        WHEN A.ALQUILER_TIPO_OPERACION IS NOT NULL THEN 'Alquiler'
+        ELSE 'Otro' 
+    END AS Tipo_Operacion,
+    COALESCE(V.VENTA_SUCURSAL, A.ALQUILER_SUCURSAL) AS Sucursal,
+    COALESCE(V.VENTA_ANIO, A.ALQUILER_ANIO) AS Anio,
+    COALESCE(V.VENTA_CUATRI, A.ALQUILER_CUATRI) AS Cuatrimestre,
+    AVG(COALESCE(V.TOTAL_COMISION, A.TOTAL_PAGOS)) AS Promedio_Comision
+FROM DATA_TEAM.BI_H_VENTA V
+FULL JOIN DATA_TEAM.BI_H_ALQUILER A
+    ON V.VENTA_SUCURSAL = A.ALQUILER_SUCURSAL
+    AND V.VENTA_ANIO = A.ALQUILER_ANIO
+    AND V.VENTA_CUATRI = A.ALQUILER_CUATRI
+GROUP BY 
+    CASE 
+        WHEN V.VENTA_TIPO_OPERACION IS NOT NULL THEN 'Venta'
+        WHEN A.ALQUILER_TIPO_OPERACION IS NOT NULL THEN 'Alquiler'
+        ELSE 'Otro' 
+    END,
+    COALESCE(V.VENTA_SUCURSAL, A.ALQUILER_SUCURSAL),
+    COALESCE(V.VENTA_ANIO, A.ALQUILER_ANIO),
+    COALESCE(V.VENTA_CUATRI, A.ALQUILER_CUATRI)
+ORDER BY Anio, Cuatrimestre, Sucursal, Tipo_Operacion;
+
+    ---------------  7
+    /* Cantidad de anuncios por tipo de inmueble, tipo de operación y rango de
 
 /* 
 DROP TABLE DATA_TEAM.BI_D_AMBIENTES
